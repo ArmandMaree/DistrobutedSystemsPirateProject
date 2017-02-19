458030153ed3e0984fac50ffdb44f912603d291d
Armand Maree <aamaree@gmail.com>
2017-02-19 12:36:12 +0200

    Pirate solves clue and sends it back to QM

--- src/pirate.py
+++ src/pirate.py
@@ -3,43 +3,256 @@
 import os
 import time
 import sys
+import json
+from time import gmtime, strftime
+from datetime import datetime
+import hashlib
+
+def printEnd(text, start="", end="\n"):
+	currTime = strftime("%Y-%m-%d %H:%M:%S.", gmtime()) + str(datetime.now().microsecond)
+	sys.stdout.write(start+ "(" + currTime + ") " + text + end)
+	sys.stdout.flush()
 
 class Pirate:
 	myId = 0
 
 	def __init__(self):
 		i = 1
+
 		while i < len(sys.argv):
-			if sys.argv[i] == "--id":
+			if sys.argv[i] == "--id" or sys.argv[i] == "-i":
 				i = i + 1
-				myId = sys.argv[i]
+				self.myId = sys.argv[i]
 			else:
-				print("Argument " + str(i) + "(" + sys.argv[i] + ") is unknown.")
+				printEnd("P: Argument " + str(i) + "(" + sys.argv[i] + ") is unknown.")
 
 			i = i + 1
 
-		print("Pirate " + str(myId) + " created!")
+		open("pirate_" + self.myId + ".chat", 'a').close()
 
 	def tellQuarterMaster(self, message):
 		sentmessage = False
-		filename = "quartermaster.chat"
+		filename = "quartermaster_" + str(self.myId) + ".chat"
 		filenameLock = filename + ".lock"
 
-		if not os.path.exists(filenameLock):
-			qmchat = open(filename, "w+")
-			qmchat.close()
+		while os.path.exists(filenameLock):
+			pass
 
 		while not sentmessage:
 			try:
 				os.rename(filename, filenameLock)
 				qmchat = open(filenameLock, "a+")
 				qmchat.write(message)
-			except OSError as e:
-				print("P(" + str(myId) + ": " + filename + " is locked.")
-				time.sleep(1)
-			finally:
+				sentmessage = True
 				qmchat.close()
 				os.rename(filenameLock, filename)
 				sentmessage = True
+				printEnd("P(" + str(self.myId) + "): Sent message to quarter master.")
+			except OSError as e:
+				if os.path.exists(filenameLock):
+					# printEnd("P(" + str(self.myId) + "): " + filename + "' is locked.")
+					pass
+				else:
+					printEnd("P(" + str(self.myId) + "): '" + filename + "' does not exist.")
+					raise e
+			finally:
+				pass
+
+	def listen(self):
+		filename = "pirate_" + str(self.myId) + ".chat"
+		filenameLock = filename + ".lock"
+		readmessage = False
+
+		while not readmessage:
+			try:
+				os.rename(filename, filenameLock)
+				qmchat = open(filenameLock, "a+")
+				message = qmchat.read()
+				readmessage = True
+
+				if message != "":
+					message = json.loads(message)
+					qmchat.close()
+					open(filename, 'w').close()
+
+					while not os.path.exists(filename):
+						pass
+						
+					os.remove(filenameLock)
+				else:
+					message = {}
+					qmchat.close()
+					os.rename(filenameLock, filename)
+			except OSError as e:
+				if os.path.exists(filenameLock):
+					# printEnd("P(" + str(self.myId) + "): '" + filename + "' is locked.")
+					pass
+				else:
+					printEnd("P(" + str(self.myId) + "): '" + filename + "' does not exist.")
+					raise e
+			finally:
+				pass
+
+		return message
+
+	def analyseMessage(self, message):
+		if message["status"] == "success":
+			if message["command"] == "solveclue":
+				result = self.solveClue(message["clue"])
+				result = {
+					"pirateId": p.myId,
+					"status": "success",
+					"request": "validate",
+					"clue": result
+				}
+				self.tellQuarterMaster(json.dumps(result))
+			else:
+				printEnd("P(" + str(self.myId) + ": Unknown command " + message["command"] + " message received form pirate " + message["pirateId"])
+		elif message["status"] == "error":
+			printEnd("P(" + str(self.myId) + "): ERROR " + str(message["code"]) + ": " + message["message"] + ".")
+		else:
+			printEnd("P(" + str(self.myId) + "): Unknown status '" + message["status"] + "' received form quartermaster")
+
+	def solveClue(self, clue):
+		printEnd("P(" + str(self.myId) + "): Solving clue. Currently at stage 1.", "", "")
+		clue = self.digInTheSand(clue)
+		printEnd("P(" + str(self.myId) + "): Solving clue. Currently at stage 2.", "\r", "")
+		clue = self.searchTheRiver(clue)
+		printEnd("P(" + str(self.myId) + "): Solving clue. Currently at stage 3.", "\r", "")
+		clue = self.crawlIntoTheCave(clue)
+		printEnd("P(" + str(self.myId) + "): Solved clue.                               ", "\r", "\n")
+		hashedClue = hashlib.md5(clue["data"]).hexdigest()
+		# printEnd("HERE IS THE HASH: " + hashedClue)
+		clue["data"] = hashedClue
+		return clue
+
+	def digInTheSand(self, clue):
+		for x in xrange(0,100):
+			clue = self.useShovel(clue)
+		
+		for x in xrange(0,200):	
+			clue = self.useBucket(clue)
+
+		for x in xrange(0,100):
+			clue = self.useShovel(clue)
+
+		return clue
+
+	def searchTheRiver(self, clue):
+		for x in xrange(0,200):	
+			clue = self.useBucket(clue)
+
+		return clue
+
+	def crawlIntoTheCave(self, clue):
+		for x in xrange(0,200):	
+			clue = self.useRope(clue)
+
+		for x in xrange(0,100):
+			clue = self.useTorch(clue)
+
+		return clue
+
+	def useShovel(self, clue):
+		clue["data"] = ''.join(sorted(clue["data"]))
+
+		if clue["data"][0].isdigit():
+			clue["data"] += "0A2B3C"
+
+		clue["data"] = clue["data"][1:]
+
+		return clue
+
+	def useBucket(self, clue):
+		newClue = ""
+
+		for c in clue["data"]:
+			if c.isdigit():
+				c = int(c)
+
+				if c > 5:
+					c = c - 2
+				else:
+					c = c * 2
+
+				c = str(c)
+
+			newClue += c
+
+		clue["data"] = newClue
+		return clue
+
+	def useRope(self, clue):
+		newClue = ""
+
+		for c in clue["data"]:
+			if c.isdigit():
+				c = int(c)
+
+				if c % 3 == 0:
+					c = "5"
+				elif c % 3 == 1:
+					c = "A"
+				elif c % 3 == 2:
+					c = "B"
+			elif c.isalpha(): #TODO: can be optimized to just an else
+				c = int(c, 16) - 10 # TODO: Check this, i think it is a mistake in the pdf
+
+				if c % 5 == 0:
+					c = "C"
+				elif c % 1 == 0:
+					c = "1"
+				elif c % 2 == 0:
+					c = "2"
+				else:
+					c = str(c)
+
+			newClue += c
+
+		clue["data"] = newClue
+		return clue
+
+	def useTorch(self, clue):
+		newClue = ""
+		sum = 0
+
+		for c in clue["data"]:
+			if c.isdigit():
+				sum += int(c)
+
+		if sum < 100:
+			sum = sum * sum
+
+		sum = str(sum)
+
+		if len(sum) < 10:
+			sum = "F9E8D7" + sum[1:]
+		else:
+			sum = sum[6:] + "A1B2C3"
+
+		clue["data"] = sum
+		return clue
+
 
 p = Pirate()
+
+done = False
+
+while not done:
+	message = {
+		"pirateId": p.myId,
+		"status": "idle",
+		"request": "clue"
+	}
+	p.tellQuarterMaster(json.dumps(message))
+	message = p.listen()
+
+	while message == {}:
+		message = p.listen()
+
+	if message["status"] == "success" and message["command"] == "shutdown":
+		done = True
+
+	p.analyseMessage(message)
+	done = True
+
--- src/quartermaster.py
+++ src/quartermaster.py
@@ -1,50 +1,205 @@
 #!/usr/bin/python
 
 import os
+import sys
 import time
 import json
 import multiprocessing # for getting number of cores
+from time import gmtime, strftime
+from datetime import datetime
+
+def printEnd(text, start="", end="\n"):
+	currTime = strftime("%Y-%m-%d %H:%M:%S.", gmtime()) + str(datetime.now().microsecond)
+	sys.stdout.write(start+ "(" + currTime + ") " + text + end)
+	sys.stdout.flush()
+
+def cleanChats():
+	os.system("rm *.chat *.chat.lock *.log *.err 2> /dev/null")
+	pass
 
 class QuarterMaster:
+	pirateIds = []
+	clues = []
+	currentClueId = 0
+
 	def __init__(self):
-		print("Quarter Master created!")
+		printEnd("Quarter Master created!")
+		printEnd("Waking Rummy...", "", "")
 		self.tellRummy("-wake")
+		printEnd("Rummy is awake.     ", "\r")
+		printEnd("Rummy is preparing...", "", "")
+		self.tellRummy("-prepare")
+		printEnd("Rummy is prepared.       ", "\r")
 
 	def createPirates(self):
+		printEnd("Getting pirates...", "", "")
 		numCpus = multiprocessing.cpu_count()
+		numCpus = 1
+		pirateData = json.loads(self.tellRummy("-add " + str(numCpus)))
+		self.pirateIds = pirateData["data"]
 
 		for x in xrange(0, numCpus):
-			os.system("./pirate.py --id " + str(x))
-			self.tellRummy("-a 1")
+			# os.spawnl(os.P_NOWAIT, "./pirate.py --id " + self.pirateIds[x] + " > " + self.pirateIds[x] + ".log " + " 2> " + self.pirateIds[x] + ".err")
+			# os.system("./pirate.py --id " + self.pirateIds[x] + " > " + self.pirateIds[x] + ".log " + " 2> " + self.pirateIds[x] + ".err")
+			# os.system("./pirate.py --id " + self.pirateIds[x])
+			os.system("./pirate.py --id " + self.pirateIds[x] + " &")
+			open("quartermaster_" + self.pirateIds[x] + ".chat", 'a').close()
+
+		printEnd("Got " + str(numCpus) + " pirates.            ", "\r")
+
+	def shipout(self):
+		printEnd("Shipping out...", "", "")
+		self.tellRummy("-shipout")
+		printEnd("Shipped out.       ", "\r")
+
+	def getClues(self):
+		printEnd("Getting clues...", "", "")
+		clueData = json.loads(self.tellRummy("-clues"))
+		clueData = clueData["data"]
+
+		for x in xrange(0,len(clueData)):
+			pirateId = clueData[x]["id"]
+			oldLength = len(self.clues)
+			self.clues.extend(clueData[x]["data"])
+
+			for y in xrange(oldLength,len(self.clues)):
+				self.clues[y]["pirateId"] = pirateId
+
+		printEnd("Got " + str(len(self.clues)) + " clues.         ", "\r")
 
 	def tellPirate(self, pirateId, message):
 		sentmessage = False
 		filename = "pirate_" + str(pirateId) + ".chat"
 		filenameLock = filename + ".lock"
 
-		if not os.path.exists(filenameLock):
-			piratechat = open(filename, "w+")
-			piratechat.close()
+		while os.path.exists(filenameLock):
+			pass
 
 		while not sentmessage:
 			try:
 				os.rename(filename, filenameLock)
 				piratechat = open(filenameLock, "a+")
 				piratechat.write(message)
-			except OSError as e:
-				print("QM: '" + filename + "' is locked.")
-			finally:
+				sentmessage = True
 				piratechat.close()
 				os.rename(filenameLock, filename)
-				sentmessage = True
-				print("QM: Message sent to pirate " + str(pirateId) + ": " + message)
+				printEnd("QM: Sent message to pirate " + pirateId + ".")
+			except OSError as e:
+				if os.path.exists(filenameLock):
+					# printEnd("QM: '" + filename + "' is locked.")
+					pass
+				else:
+					printEnd("QM: '" + filename + "' does not exist.")
+					raise e
+			finally:
+				pass
+				#printEnd("QM: Message sent to pirate " + str(pirateId) + ".")
 
 	def tellRummy(self, message):
-		rummyResponse = json.loads(os.popen("./rummy.pyc " + message).read())
-		print "R (" + rummyResponse["status"] + "): ", rummyResponse["message"]
+		returnMsg = os.popen("./rummy.pyc " + message).read()
+		rummyResponse = json.loads(returnMsg)
 		return json.dumps(rummyResponse)
 
-print("Let's find that treasure!\n")
-qm = QuarterMaster()
-qm.createPirates()
-qm.tellPirate(1, "hello")
+	def listen(self):
+		done = False
+		response = {
+			"proceed": True
+		}
+
+		while not done:
+			for x in xrange(0,len(self.pirateIds)):
+				filename = "quartermaster_" + str(self.pirateIds[x]) + ".chat"
+				filenameLock = filename + ".lock"
+				readmessage = False
+
+				if os.path.exists(filename):
+					while not readmessage:
+						try:
+							os.rename(filename, filenameLock)
+							piratechat = open(filenameLock, "a+")
+							message = piratechat.read()
+							readmessage = True
+
+							if message != "":
+								message = json.loads(message)
+								response = self.respondToPirateRequest(message)
+								piratechat.close()
+								open(filename, 'w').close()
+
+								while not os.path.exists(filename):
+									pass
+									
+								os.remove(filenameLock)
+							else:
+								piratechat.close()
+								os.rename(filenameLock, filename)
+						except OSError as e:
+							if os.path.exists(filenameLock):
+								# printEnd("QM: '" + filename + "' is locked.")
+								pass
+							else:
+								printEnd("QM: '" + filename + "' does not exist.")
+								raise e
+						finally:
+							pass
+
+				if response["proceed"] == False:
+					done = True
+
+	def respondToPirateRequest(self, message):
+		response = {
+			"proceed": True
+		}
+
+		if message["status"] == "idle":
+			if message["request"] == "clue":
+				sendMessage = {
+					"status": "success",
+					"command": "solveclue",
+					"clue": self.clues[self.currentClueId]
+				}
+				self.currentClueId = self.currentClueId + 1
+				self.tellPirate(message["pirateId"], json.dumps(sendMessage))
+				# printEnd("QM: Sent clue[" + str(self.currentClueId - 1) + "] to pirate " + message["pirateId"] + ".")
+			else:
+				sendMessage = {
+					"status": "error",
+					"code": 401,
+					"message": "Unknown idle message."
+				}
+				self.tellPirate(message["pirateId"], json.dumps(sendMessage))
+				printEnd("QM: Unknown idle message '" + message["request"] + "' received form pirate " + message["pirateId"])
+		elif message["status"] == "success":
+			if message["request"] == "validate":
+				response["proceed"] = False # debug
+				printEnd("QM: Received validation code: " + message["clue"]["data"])
+			else:
+				sendMessage = {
+					"status": "error",
+					"code": 402,
+					"message": "Unknown success message."
+				}
+				self.tellPirate(message["pirateId"], json.dumps(sendMessage))
+				printEnd("QM: Unknown idle message '" + message["request"] + "' received form pirate " + message["pirateId"])
+		else:
+			sendMessage = {
+				"status": "error",
+				"code": 410,
+				"message": "Unknown status"
+			}
+			self.tellPirate(message["pirateId"], json.dumps(sendMessage))
+			printEnd("QM: Unknown status '" + message["status"] + "' received form pirate " + message["pirateId"])
+
+		return response
+
+try:
+	cleanChats()
+	printEnd("Let's find that treasure!\n")
+	qm = QuarterMaster()
+	qm.createPirates()
+	qm.shipout()
+	qm.getClues()
+	qm.listen()
+finally:
+	# cleanChats() # must always be the last statement
+	pass
